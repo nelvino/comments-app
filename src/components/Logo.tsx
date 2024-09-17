bb2caaf0f023fd4610dab23f79869da89a828330
Nico Elvino <nicoelvino@gmail.com>
2024-09-17 16:36:01 +1000

    Nico logo added

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,7 +1,7 @@
 export default function Logo() {
   return (
     <a href={`${import.meta.env.BASE_URL}`} className="logo">
-      <img src={`${import.meta.env.BASE_URL}img/NicoLogo.png`} alt="logo" />
+      <img src={`${import.meta.env.BASE_URL}/src/img/NicoLogo.png`} alt="logo" />
     </a>
   );
 }
