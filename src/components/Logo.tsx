6a51e7e988f19c35c997129e00c3887f255da025
Nico Elvino <nicoelvino@gmail.com>
2024-09-17 14:51:55 +1000

    context API changed to Zustand

---
+++ src/components/Logo.tsx
@@ -0,0 +1,7 @@
+export default function Logo() {
+  return (
+    <a href="/" className="logo">
+      <img src="/NicoLogo.png" alt="logo" />
+    </a>
+  );
+}
---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,18 @@
+export default function Footer() {
+    return (
+      <footer className="footer">
+        <small>
+          <p>
+            &copy; Copyright by{" "}
+            <a target="_blank" href="https://nicoelvinoportfolio.netlify.app/">
+              Nico Elvino
+            </a>{" "}
+            This is part of my portfolio.
+          </p>
+          <p>
+            <span className="u-bold u-italic">All credits</span> to me!{" "}
+          </p>
+        </small>
+      </footer>
+    );
+  }
