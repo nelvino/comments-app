adfd0dc0fc9edc38562cc5cb13acd5aad9c0e5af
Nico Elvino <nicoelvino@gmail.com>
2024-09-17 16:21:50 +1000

    deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/comments-app/',  // Set the base path for GitHub Pages
+  base: '/comments-app/',
   plugins: [react()],
 })
